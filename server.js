c93790d4b3493c9add353c37a5701cc57ce74c5a
Abhishek Varun <varunabhi16@gmail.com>
2017-08-08 23:34:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/arical-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
